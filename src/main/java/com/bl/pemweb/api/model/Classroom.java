27ee6104277abf535ef79ff4f184481f856d9257
andry <andry.sunandar@gmail.com>
2017-11-11 06:44:25 +0700

    add classroom model
    #add

---
+++ src/main/java/com/bl/pemweb/api/model/Classroom.java
@@ -0,0 +1,39 @@
+
+package com.bl.pemweb.api.model;
+
+/**
+ *
+ * Author andry on 11/11/17.
+ */
+public class Classroom {
+
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(float humidity) {
+        this.humidity = humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(float pressure) {
+        this.pressure = pressure;
+    }
+    
+    
+}
