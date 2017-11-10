b31540da76122200536668cc9f5d302d4e799881
andry <andry.sunandar@gmail.com>
2017-11-11 06:14:57 +0700

    add websocket config
    #update

---
+++ src/main/java/com/bl/pemweb/api/config/WebSocketConfig.java
@@ -0,0 +1,40 @@
+/**
+ * Innovez-One, Proprietary Software Cloud Communications
+ * Copyright (c) 2015, Innovez-One and individual contributors
+ * by the @authors tag.
+ * <p>
+ * This program is Proprietary Software: you can not redistribute it and/or modify
+ * without license from Innovez-One.
+ * <p>
+ * Website : http://www.innovez-one.com/
+ * Report bugs to <techsupport@innovez-one.com>.
+ * Copyright (C) 2015 PT. Innovez-One. All rights reserved.
+ */
+package com.bl.pemweb.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+/**
+ *
+ * Author andry on 11/11/17.
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config){
+        config.enableSimpleBroker("/topic","/queue");
+        config.setApplicationDestinationPrefixes("/ubl");
+
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+        stompEndpointRegistry.addEndpoint("/temperature");
+    }
+}
